e53a7486aa011b9dc830e5d7e19bf644888c61dc
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-20 02:30:19 +0530

    Server:Auth route

--- server/server.js
+++ server/server.js
@@ -17,24 +17,45 @@ app.use(cookieParser());
 
 const { User } = require("./models/user");
 
+//Middlewares
+
+const { auth } = require("./middleware/auth");
+
 //==================================
 //        Users
 //==================================
 
+//AUTH ROUTE
+
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
+  });
+});
+
+//REGISTER USER
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     res.status(200).json({
-      success: true,
-      userData: doc
+      success: true
     });
   });
 });
 
+//LOGIN USER
 app.post("/api/users/login", (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
+  //prettier-ignore
+  User.findOne({ 'email': req.body.email }, (err, user) => {
     if (!user)
       return res.json({
         loginSucess: false,
@@ -47,10 +68,8 @@ app.post("/api/users/login", (req, res) => {
 
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
-        res
-          .cookie("w_auth", user.token)
-          .status(200)
-          .json({ loginSucess: true });
+        //prettier-ignore
+        res.cookie('w_auth', user.token,{httpOnly:false}).status(200).json({ loginSucess: true });
       });
     });
   });
