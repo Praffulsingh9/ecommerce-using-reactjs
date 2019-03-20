28081f233d5e61dea35605d018042b6072826b63
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-20 20:05:28 +0530

    Client:Setting up Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom";
 import "./Resources/css/styles.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducer from "./reducers";
+
+const store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider
+    store={store(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
