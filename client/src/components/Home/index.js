7390a4694e93564e16baf603ed452f386b9c2958
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-21 16:57:54 +0530

    Client:Setting up redux for Home Component

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import HomeSlider from "./home_slider";
 import HomePromotion from "./home_promotions";
+import { connect } from "react-redux";
+import {
+  getProductsByArrival,
+  getProductsBySell
+} from "../../actions/product_actions";
 
 class Home extends Component {
+  componentDidMount() {
+    this.props.getProductsBySell();
+    this.props.getProductsByArrival();
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +23,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+  products: state.products
+});
+export default connect(
+  mapStateToProps,
+  { getProductsByArrival, getProductsBySell }
+)(Home);
